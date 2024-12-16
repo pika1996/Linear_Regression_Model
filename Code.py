a8335dbe85a2bece507fa650ee96630aa1dba8f0
pika1996 <priyankamhadeshri@gmail.com>
2024-12-16 19:47:35 +0900

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.utils import resample
-
+import kagglehub
 
 # Load Kaggle Dataset after local download
 # Dataset used: Swedish Auto Insurance Dataset
-path = "dataset/auto-insurance.csv"
+path = kagglehub.dataset_download("redwankarimsony/auto-insurance-in-sweden")
 data = pd.DataFrame({ 'claims': [108, 19, 13, 124, 40, 57, 23, 14, 45, 10, 5, 48, 11, 23, 7, 2, 24, 6, 3, 23, 6, 9, 9, 3, 29, 7, 4, 20, 7, 4, 0, 25, 6, 5, 22, 11, 61, 12, 4, 16, 13, 60, 41, 37, 55, 41, 11, 27, 8, 3, 17, 13, 13, 15, 8, 29, 30, 24, 9, 31, 14, 53, 26],
                      'payment': [392.5, 46.2, 15.7, 422.2, 119.4, 170.9, 56.9, 77.5, 214, 65.3, 20.9, 248.1, 23.5, 39.6, 48.8, 6.6, 134.9, 50.9, 4.4, 113, 14.8, 48.7, 52.1, 13.2, 103.9, 77.5, 11.8, 98.1, 27.9, 38.1, 0, 69.2, 14.6, 40.3, 161.5, 57.2, 217.6, 58.1, 12.6, 59.6, 89.9, 202.4, 181.3, 152.8, 162.8, 73.4, 21.3, 92.6, 76.1, 39.9, 142.1, 93, 31.9, 32.1, 55.6, 133.3, 194.5, 137.9, 87.4, 209.8, 95.5, 244.6, 187.5]
                     })
